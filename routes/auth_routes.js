29f2a47b5f6f18c821a01297f4ba557a09797d54
juano-vazquez <juano.vazquez@icloud.com>
2025-02-16 20:24:50 -0600

    Delete routes directory

--- routes/auth_routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/auth_controller');
-const validationRequest = require('../config/common/middlewares/validation_request');
-
-router.post("/login", authController.authValidators, validationRequest, authController.login);
-router.all("/logout", authController.logout);
-
-module.exports = router;
